3bb86b8f86be601ba937feefebb73b45901f4c2b
Emilse Arias <emilsearias@gmail.com>
2026-06-03 21:38:13 -0300

    agregamos dos enpoint de prueba

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 // const express = require('express')
 
 const app = express()
 const PORT = 3000
 
+// middlewares
+app.use(cors()); //permite conexiones remotas
+app.use(express.json()) // permite interpretar los datos que lleguen en la solicitud o request en formato json
+app.use(morgan('dev'))
+
+// area de logica
 console.log('Este es el segundo mensaje de este backend 🥐')
 console.log('nueva prueba')
+
+// diseñamos los endpoints
+// http://localhost:3000/api/saludo
+app.get('/api/saludo', (req, res)=>{
+    res.json('Bienvenidos a nuestro backend')
+})
+
+app.get('/api/adios', (req, res)=>{
+    res.json('Nos vamos')
+})
 
 app.listen(PORT, ()=>{
     console.log(`Servidor activo en el puerto: ${PORT}`)
